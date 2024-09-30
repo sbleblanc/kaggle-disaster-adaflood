d98a707482be1c6b030ca67fa1bfd780997c1bd0
Samuel Beland-Leblanc <samuel@sbl-dev.ca>
2024-09-30 19:37:38 -0400

    Updates uv's version and regenerates requirements.txt as universal

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 let
   nixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f1bad50880bae73ff2d82fafc22010b4fc097a9c.tar.gz") { config = {allowUnfree = true;}; overlays = []; }; #nixos-24.05 2024/08/20";
+  nixpkgs-unstable = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ef7226d68ba45b2de3e428e5d4bb4532caffec7b.tar.gz") { config = {allowUnfree = true;}; overlays = []; }; #nixos-unstable 2024/09/30";
   pkgs = nixpkgs.pkgs;
   nixpkgs-python = import (fetchTarball "https://github.com/cachix/nixpkgs-python/archive/refs/heads/main.zip");
   python = nixpkgs-python.packages.x86_64-linux."3.10.14";
@@ -12,7 +13,7 @@ pkgs.mkShell {
 
   packages = [
     python
-    pkgs.uv 
+    nixpkgs-unstable.pkgs.uv
     pkgs.cudaPackages.cudatoolkit
     pkgs.zlib
     pkgs.pkg-config
